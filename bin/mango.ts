d7812a23c5b3f669c325ee1e676f07cbc1daafee
zacariec <zacariec@github.com>
2023-10-30 10:30:22 +1100

    Updated config command and passed through correct flags. Updated create command

--- bin/mango.ts
+++ bin/mango.ts
@@ -1,6 +1,24 @@
-import { Mango } from "./utils/cli";
+import { configure } from "./commands/config";
+import { create } from "./commands/create";
+import { Command, CLI, Flag } from "./utils/cli";
+
+const mango = new CLI(Bun.argv);
 
 
-const mango = new Mango(Bun.argv);
+const configCommand = new Command("config", configure, "")
+    .flag(new Flag("themeid", "Required: The Storefront themeid for this environment", "t", true))
+    .flag(new Flag("storefront", "Required: The Storefront url for this environment eg: \"mystorefront.myshopify.com\"", "s", true))
+    .flag(new Flag("password", "Required: The Storefront application password to utilise for changes to this environment", "p", true))
+    .flag(new Flag("ignores", "Optional: The ignores file containing patterns for files/directories to ignore", "i"))
+    .flag(new Flag("environment", "Optional: The environment name to use instead of the default \"development\"", "e"))
+    .flag(new Flag("directory", "Optional: The working directory to use instead of the default", "d"));
+
+const createCommand = new Command("create", create, "")
+
+
+mango
+  .add(new Command("watch", () => null, ""))
+  .add(createCommand)
+  .add(configCommand);
 
 mango.run();
