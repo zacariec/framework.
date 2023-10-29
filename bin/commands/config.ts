e877f70870505d798c52127f20fb36f91d3f2b4b
zacariec <zacariec@github.com>
2023-10-30 10:30:22 +1100

    Fixed some bugs with config command.

--- bin/commands/config.ts
+++ bin/commands/config.ts
@@ -16,7 +16,7 @@ interface TomlConfig {
 
 export async function configure(
   this: CLI,
-  args: Map<keyof Config, Config[keyof Config]>
+  args: Map<keyof Config, Config[keyof Config]>,
 ) {
   const configPath = `${process.cwd()}/mango.toml`;
   const configFile = Bun.file(configPath);
@@ -24,47 +24,55 @@ export async function configure(
   let content = "";
 
   try {
-    const isNewConfiguration: boolean = await new Promise(async (resolve, reject): Promise<any> => {
-      if (exists === true) {
-        const mod = await import(configPath);
-        const config: TomlConfig = mod.default;
+    const isNewConfiguration: boolean = await new Promise(
+      async (resolve, reject): Promise<any> => {
+        if (exists === true) {
+          const mod = await import(configPath);
+          const config: TomlConfig = mod.default;
 
-        let key = args.get("environment");
+          let key = args.get("environment");
 
-        // it's a new configuration.
-        if (config === undefined) {
-          return resolve(true);
-        }
+          // it's a new configuration.
+          if (config === undefined) {
+            return resolve(true);
+          }
 
-        // if a key is undefined, it means an environment wasn't passed as a flag and therefore is "development"
-        if (key === undefined) {
-          key = "development";
-        }
+          // if a key is undefined, it means an environment wasn't passed as a flag and therefore is "development"
+          if (key === undefined) {
+            key = "development";
+          }
 
-        const environmentExists = config[key];
+          const environmentExists = config[key];
 
-        if (environmentExists === undefined) {
+          if (environmentExists === undefined) {
+            return resolve(false);
+          }
+
+          // reject the promise if the environment exists, the user will need to supply the environment flag.
+          if (Object.keys(environmentExists).length !== 0) {
+            return reject();
+          }
+
+          // if we make it here we can continue the execution flow by succesfully resolving the promise.
           return resolve(false);
         }
-
-        // reject the promise if the environment exists, the user will need to supply the environment flag.
-        if (Object.keys(environmentExists).length !== 0) {
-          return reject();
-        }
-
-        // if we make it here we can continue the execution flow by succesfully resolving the promise.
-        return resolve(false);
-      }
-    });
+      },
+    );
 
     content += `
 ${isNewConfiguration === false ? await configFile.text() : ""}
-[${args.get("environment") !== undefined ? args.get("environment") : "development"}]
+[${args.get("environment") !== undefined
+        ? args.get("environment")
+        : "development"
+      }]
 themeid = ${args.get("themeid")}
 storefront = "${args.get("storefront")}"
 password = "${args.get("password")}"
 ${args.get("ignores") !== undefined ? `ignores = "${args.get("ignores")}"` : ``}
-${args.get("directory") !== undefined ? `directory = "${args.get("directory")}"` : ``}
+${args.get("directory") !== undefined
+        ? `directory = "${args.get("directory")}"`
+        : ``
+      }
 
 `;
 
@@ -74,6 +82,12 @@ ${args.get("directory") !== undefined ? `directory = "${args.get("directory")}"`
       stderr(String(err));
     }
   } catch (err) {
-    stderr(`${args.get("environment") === undefined ? "development" : args.get("environment")} environment already exists, please supply the --environment or -e flag to mango config`);
+    stderr(
+      `${args.get("environment") === undefined
+        ? "development"
+        : args.get("environment")
+      } environment already exists, please supply the --environment or -e flag to mango config`,
+    );
   }
 }
+
