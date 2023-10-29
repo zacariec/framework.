b4a157a041031bb201899c9ce81ab9d249229a9d
zacariec <zacariec@github.com>
2023-10-30 10:30:22 +1100

    Added Explicit type for StdWriter Options, added stdout method to CLI class.

--- bin/utils/cli.ts
+++ bin/utils/cli.ts
@@ -2,13 +2,17 @@ import { stderr } from "./utils";
 
 type Args = string[];
 type Flags = string[][];
+type StdWriter = { highWaterMark: number | undefined };
 
 interface ICli {
   _pathToBun: string;
   _pathToExecutable: string;
   command: string;
   commands: Command[];
-  stderr: (arg0: string, options: { highWaterMark: number | undefined }) => void;
+  stderr: (
+    arg0: string,
+    options: { highWaterMark: number | undefined }
+  ) => void;
   run: (arg0?: string, arg1?: (...args: any) => any) => void;
 }
 
@@ -30,7 +34,24 @@ export class CLI implements ICli {
     this.commands = [];
     this.params = args.splice(3);
     this.subcommand = this.params[0];
-    this.flags = this.params.filter((flag) => flag.substring(0, 1) === "-").filter(Boolean).map((flag) => flag.split("="));
+    this.flags = this.params
+      .filter((flag) => flag.substring(0, 1) === "-")
+      .filter(Boolean)
+      .map((flag) => flag.split("="));
+  }
+
+  /**
+   * Initializes a stdout writer start & end.
+   * @param input - Takes a string to write to the stdout & outputs it to console.
+   * @param options - Takes an object that matches the stdout.writer object.
+   * @returns void
+   */
+  stdout(input: string, options?: StdWriter): void {
+    const writer = Bun.stdout.writer(options);
+
+    writer.start();
+    writer.write(`${input}\n`);
+    writer.end();
   }
 
   /**
@@ -39,7 +60,7 @@ export class CLI implements ICli {
    * @param options - Takes an object that matches the stderr.writer object.
    * @returns void
    */
-  stderr(input: string, options?: { highWaterMark: number | undefined }): void {
+  stderr(input: string, options?: StdWriter): void {
     const writer = Bun.stderr.writer(options);
 
     writer.start();
@@ -48,34 +69,36 @@ export class CLI implements ICli {
   }
 
   /**
-    * Registers a command to an array of commands on the constructor.
-    * @param command - The Command class to register
-    * @returns this
-    */
+   * Registers a command to an array of commands on the constructor.
+   * @param command - The Command class to register
+   * @returns this
+   */
   add(command: Command): this {
     this.commands.push(command);
     return this;
-
   }
 
   /**
-    * Runs the invoked CLI, checking against commands that have been
-    * registered into the constructor.
-    * @returns Command | Cli - returns the Command if found or the CLI instance - this is useless
-    */
+   * Runs the invoked CLI, checking against commands that have been
+   * registered into the constructor.
+   * @returns Command | Cli - returns the Command if found or the CLI instance - this is useless
+   */
   run(): Command | this {
-
     if (this.command !== "mango") {
       this.stderr(`Use "mango" to invoke mango actions.`);
       return this;
     }
 
     if (this.commands.length === 0) {
-      this.stderr("No commands configured, register commands against the constructed class.");
+      this.stderr(
+        "No commands configured, register commands against the constructed class."
+      );
       return this;
     }
 
-    const command = this.commands.find((command) => command.command === this.subcommand);
+    const command = this.commands.find(
+      (command) => command.command === this.subcommand
+    );
 
     if (!command) {
       this.stderr(`Command "${this.subcommand}": not found, or doesn't exist.`);
@@ -93,7 +116,11 @@ export class Command {
   flags: Flag[];
   requiredFlags: Flag[];
 
-  constructor(command: string, callback: (arg0?: any) => any, description?: string) {
+  constructor(
+    command: string,
+    callback: (arg0?: any) => any,
+    description?: string
+  ) {
     this.command = command;
     this.description = description;
     this.callback = callback;
@@ -116,14 +143,25 @@ export class Command {
     const missingFlags: string[] = [];
     const invalidFlags: string[] = [];
 
-
-    if (this.requiredFlags.length > 0 && mappedFlags !== undefined && mappedFlags.length === 0) {
-      stderr(`Required flag(s): "${this.requiredFlags.map((flag) => flag.flag).join(" ")}" ${this.requiredFlags.length > 1 ? "were" : "was"} not supplied and is marked as required`);
+    if (
+      this.requiredFlags.length > 0 &&
+      mappedFlags !== undefined &&
+      mappedFlags.length === 0
+    ) {
+      stderr(
+        `Required flag(s): "${this.requiredFlags
+          .map((flag) => flag.flag)
+          .join(" ")}" ${
+          this.requiredFlags.length > 1 ? "were" : "was"
+        } not supplied and is marked as required`
+      );
       return this;
     }
 
     this.requiredFlags.forEach((requiredFlag) => {
-      if (mappedFlags?.find((flag) => requiredFlag.flag === flag) === undefined) {
+      if (
+        mappedFlags?.find((flag) => requiredFlag.flag === flag) === undefined
+      ) {
         missingFlags.push(requiredFlag.flag);
       }
     });
@@ -136,7 +174,11 @@ export class Command {
     });
 
     if (invalidFlags.length !== 0) {
-      stderr(`Invalid flag(s): "${invalidFlags.join(" ")}" not required by mango "${this.command}"`);
+      stderr(
+        `Invalid flag(s): "${invalidFlags.join(" ")}" not required by mango "${
+          this.command
+        }"`
+      );
       return this;
     }
 
@@ -155,7 +197,12 @@ export class Flag {
   required: boolean;
   short?: string | undefined;
 
-  constructor(flag: string, descriptor: string, short?: string | undefined, required = false) {
+  constructor(
+    flag: string,
+    descriptor: string,
+    short?: string | undefined,
+    required = false
+  ) {
     this.flag = `--${flag}`;
     this.descriptor = descriptor;
 
