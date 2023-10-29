e243599464d6c4268c2528e415a97d9c73f2ab13
zacariec <zacariec@github.com>
2023-10-30 10:30:22 +1100

    Created basic help option

--- bin/commands/help.ts
+++ bin/commands/help.ts
@@ -1,5 +1,10 @@
 import type { CLI } from "../utils/cli";
+import { stdout } from "../utils/utils";
 
-export function help(this: CLI) {
-  console.log(this);
+export function help(this: CLI): void {
+  return stdout(
+`Usage: mango [subcommand] [flags]\n
+Commands:
+  ${this.commands.map((command) => `${command.command}      ${command.description}\n`)}
+`);
 }
